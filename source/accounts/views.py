5af18616c1218f1bab5286f28d25f9d6c01c604a
Chinara <nishanova73@mail.ru>
2022-02-14 08:13:29 +0600

    created generic create view for signing in

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,45 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+from django.urls import reverse
+from django.views.generic import CreateView
+from accounts.forms import MyUserCreationForm
 
 
-# Create your views here.
+class RegisterView(CreateView):
+    model = User
+    template_name = "registration.html"
+    form_class = MyUserCreationForm
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect(self.get_success_url())
+
+    def get_success_url(self):
+        next_url = self.request.GET.get('next')
+        if not next_url:
+            next_url = self.request.POST.get('next')
+        if not next_url:
+            next_url = reverse('webapp:main_page')
+        return next_url
+
+
+# def register_view(request):
+#     form = MyUserCreationForm()
+#     if request.method == "POST":
+#         form = MyUserCreationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             url = request.GET.get("next")
+#             if url:
+#                 return redirect(url)
+#             return redirect("webapp:main_page")
+#     return render(request, "registration.html", {"form":form})
+
+
 def login_view(request):
     if request.user.is_authenticated:
         return redirect('webapp:index')
