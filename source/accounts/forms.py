49b8ef98c3192abc7ee3b0006653265865d692e4
Chinara <nishanova73@mail.ru>
2022-02-24 16:10:59 +0600

    hw-61 created change password and update profile views

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
 
+from accounts.models import Profile
 
 class MyUserCreationForm(UserCreationForm):
     email = forms.EmailField(label="Email", required=True)
@@ -11,3 +12,46 @@ class MyUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("email", "first_name", "last_name")
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("avatar", "birth_date")
+
+
+class PasswordChangeForm(forms.ModelForm):
+    password = forms.CharField(label="New password", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm the password", widget=forms.PasswordInput, strip=False)
+    old_password = forms.CharField(label="Old password", strip=False, widget=forms.PasswordInput)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Passwords are not similar!')
+        return password_confirm
+
+    def clean_old_password(self):
+        old_password = self.cleaned_data.get('old_password')
+        if not self.instance.check_password(old_password):
+            raise forms.ValidationError('Old password is not valid!')
+        return old_password
+
+    def save(self, commit=True):
+        user = self.instance
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+
+        return user
+
+    class Meta:
+        model = User
+        fields = ['password', 'password_confirm', 'old_password']
